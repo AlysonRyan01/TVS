2d251b30ddbb535a0c097330f87bd39410092fdb
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-04-04 15:45:28 -0300

    .

---
+++ TVS.web/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components;
+
+namespace TVS.web.Pages;
+
+public partial class Home : ComponentBase
+{
+    private bool _open;
+
+    
+    
+    
+    
+    private void OpenDrawer()
+    {
+        _open = true;
+    }
+}
