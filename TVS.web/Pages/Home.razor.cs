b56649e3abb5fcc860d8236a4d694bca48bee3fe
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-04-08 17:28:10 -0300

    entre em contato

--- TVS.web/Pages/Home.razor.cs
+++ TVS.web/Pages/Home.razor.cs
@@ -1,14 +1,33 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using TVS.Core.Requests.Contact;
 
 namespace TVS.web.Pages;
 
 public partial class Home : ComponentBase
 {
     private bool _open;
+    private bool FormIsBusy;
+    public ContactRequest Contact { get; set; } = new();
 
-    
-    
-    
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private async Task SendForm()
+    {
+        FormIsBusy = true;
+        try
+        {
+            Snackbar.Add("Email enviado com sucesso! Aguarde o retorno", Severity.Success);
+        }
+        catch (Exception e)
+        {
+            Snackbar.Add(e.Message, Severity.Error);
+        }
+        finally
+        {
+            FormIsBusy = false;
+        }
+    }
     
     private void OpenDrawer()
     {
