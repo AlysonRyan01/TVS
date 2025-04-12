8f1ca4fe7f2161d7eeb2e2131c8e19fa0f312693
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-04-12 12:43:22 -0300

    add robots.txt

--- TVS.web/Program.cs
+++ TVS.web/Program.cs
@@ -2,7 +2,6 @@ using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using TVS.Core;
 using TVS.Core.Services;
 using TVS.web;
 using TVS.web.Services;
